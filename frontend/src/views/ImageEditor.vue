2dc2b2020854807ec2a420d53b2478a7230710c1
JPCG-27 <jpcg27.2@gmail.com>
2025-01-27 16:19:04 +0000

    Troquei para o link primecog

--- frontend/src/views/ImageEditor.vue
+++ frontend/src/views/ImageEditor.vue
@@ -633,7 +633,7 @@ const prevImage = () => {
 };
 
 const connectWebSocket = () => {
-  ws = new WebSocket('wss' + endpoints.project.substring(4) + '/ws');
+  ws = new WebSocket('wss' + endpoints.project.substring('https'.size()) + '/ws');
    ("WS LINK " + ws.url);
   ws.onmessage = (event) => {
      ("Received message: " + event)
